55ceee1d4bdf5545476eea828aecf6db9aab9335
TheKr1d <epifanov.vv.spb@gmail.com>
2022-03-10 10:23:00 +0300

    debag

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,9 +8,4 @@ test('gendiffTest', () => {
   const correctAnswer = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
 
   expect(gendiff(test1File1, test1File2)).toBe(correctAnswer);
-
-  const test2File1 = '/home/user/hexlet-projects/frontend-project-lvl2/__fixtures__/file1.json';
-  const test2File2 = '/home/user/hexlet-projects/frontend-project-lvl2/__fixtures__/file2.json';
-
-  expect(gendiff(test2File1, test2File2)).toBe(correctAnswer);
 });
--- src/functions.js
+++ src/functions.js
@@ -4,8 +4,13 @@ import * as fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const way = process.cwd();
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
+const readFixtureFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
 
-export { readFile, getFixturePath };
+const getPath = (filename) => path.resolve(way, filename);
+const readFile = (filename) => fs.readFile(getPath(filename), 'utf-8');
+
+
+export { readFixtureFile, getFixturePath, getPath, readFile };
