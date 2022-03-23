8f55abb2b7a395f37bc508b423f6a8c2b5752447
TheKr1d <epifanov.vv.spb@gmail.com>
2022-03-23 10:46:30 +0300

    step 6

--- src/formatter.js
+++ src/formatter.js
@@ -1,37 +1,31 @@
 import _ from 'lodash';
 
-const iterObj = (obj, level) => {
+const iterObj = (node, level) => {
   const subString = '  ';
   const sub = subString.repeat(level);
-  const result = Object.entries(obj).map(([key, value]) => {
-    if (_.has(obj, value)) {
+  if (!_.isObject(node)) {
+    return `${node}`;
+  }
+  const result = Object.entries(node).flatMap(([key, value]) => {
+    if (_.isObject(value)) {
       return `${sub}  ${key}: ${iterObj(value, level + 2)}`;
     }
-    return `${value}`;
-  })
-  return result;
+    return `${sub}  ${key}: ${value}`;
+  });
+  return ['{', ...result, `${subString.repeat(level - 1)}}`].join('\n');
 };
 const stylish = (tree, subString = '  ') => {
   const iter = (node, level) => {
     const sub = subString.repeat(level);
     const children = node.children;
-    if (children === undefined) {
-      return `${node.value}`;
-    }
     const result = children.flatMap((child) => {
-      if (_.isObject(child.value1)) {
-        return `${sub}${child.action} ${child.name}: ${iterObj(child.value1, level + 2)}`;
+      if (_.has(child, 'children')) {
+        return `${sub}${child.action} ${child.name}: ${iter(child, level + 2)}`
       }
-      if (_.isObject(child.value2)) {
-        return `${sub}${child.action} ${child.name}: ${iterObj(child.value2, level + 2)}`;
+      if (_.has(child, 'del') && _.has(child, 'add')) {
+        return [`${sub}${child.del} ${child.name}: ${iterObj(child.value1, level + 2)}`, `${sub}${child.add} ${child.name}: ${iterObj(child.value2, level + 2)}`]
       }
-      if (_.isObject(child.value)) {
-        return `${sub}${child.action} ${child.name}: ${iterObj(child.value, level + 2)}`;
-      }
-      if (_.has(child, 'value2')) {
-        return [`${sub}${child.del} ${child.name}: ${child.value1}`, `${sub}${child.add} ${child.name}: ${child.value2}`];
-      }
-      return [`${sub}${child.action} ${child.name}: ${iter(child, level + 2)}`];
+      return `${sub}${child.action} ${child.name}: ${iterObj(child.value, level + 2)}`;
     });
   return ['{', ...result, `${subString.repeat(level - 1)}}`].join('\n');
  }
