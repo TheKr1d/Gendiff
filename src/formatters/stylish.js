ddb37776220c897d52e6e9796f5f59f3d76cfb3d
TheKr1d <epifanov.vv.spb@gmail.com>
2022-04-08 16:41:32 +0300

    stylish debag

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,36 +1,34 @@
 import _ from 'lodash';
 
+const indent = (level) => '  '.repeat(level);
+
 const stringify = (node, level) => {
-  const subString = '  ';
-  const sub = subString.repeat(level);
+  const sub = indent(level);
   if (!_.isObject(node)) {
     return node;
   }
-  const result = Object.entries(node).flatMap(([key, value]) => {
-    return `${sub}  ${key}: ${stringify(value, level + 2)}`;
-  });
-  return ['{', ...result, `${subString.repeat(level - 1)}${sub}}`].join('\n');
+  const result = Object.entries(node).flatMap(([key, value]) => `${sub}  ${key}: ${stringify(value, level + 2)}`);
+  return ['{', ...result, `${indent(level - 1)}}`].join('\n');
 };
 const stylish = (node, level = 1) => {
-    const subString = '  ';
-    const sub = '  '.repeat(level);
-      const { name } = node;
-      switch (node.action) {
-        case 'HEAD':
-          return `{\n${node.children.map(child => stylish(child, level)).join('\n')}\n}`;
-        case 'perent':
-          return `${sub}  ${node.name}: {${node.children.map(child => stylish(child, level + 2)).join('\n')}${sub}`;
-        case 'updated':
-          return [
-          `${sub}- ${name}: ${stringify(node.value1, level + 2)}\n${sub}+ ${name}: ${stringify(node.value2, level + 2)}`
-        ];
-        case 'added':
-          return `\n${sub}+ ${name}: ${stringify(node.value, level + 2)}`;
-        case 'removed':
-          return `\n${sub}- ${name}: ${stringify(node.value, level + 2)}`;
-        default:
-          return `\n${sub}  ${name}: ${stringify(node.value, level + 2)}`;
-      }
-  };
+  const sub = indent(level);
+  const { name } = node;
+  switch (node.action) {
+    case 'HEAD':
+      return `{\n${node.children.map((child) => stylish(child, level)).join('\n')}\n}`;
+    case 'perent':
+      return `${sub}  ${node.name}: {\n${node.children.map((child) => stylish(child, level + 2)).join('\n')}\n${indent(level + 1)}}`;
+    case 'updated':
+      return [
+        `${sub}- ${name}: ${stringify(node.value1, level + 2)}\n${sub}+ ${name}: ${stringify(node.value2, level + 2)}`,
+      ];
+    case 'added':
+      return `${sub}+ ${name}: ${stringify(node.value, level + 2)}`;
+    case 'removed':
+      return `${sub}- ${name}: ${stringify(node.value, level + 2)}`;
+    default:
+      return `${sub}  ${name}: ${stringify(node.value, level + 2)}`;
+  }
+};
 
 export default stylish;
